2751466448c889ab2cc13454b35e9f39f89ea414
Fajrikun Makalalag <S2210271@student.unklab.ac.id>
2025-06-22 03:38:55 +0800

    mmm

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import re
+import os
 
 app = Flask(__name__)
+CORS(app) # Ini akan mengizinkan request dari semua origin
 
 # Simple keyword-based classifier
 CATEGORY_KEYWORDS = {
